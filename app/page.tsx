4bddee265aec6134c5b42fee98c26ccf126a6357
Musthafa Faishal <musthafafaishal@gmail.com>
2024-05-14 00:23:22 +0700

    feat: implement navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,48 +1,19 @@
-export default function Home() {
+import Navbar from '@/components/molecules/Navbar';
+
+async function getLogoContent() {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_CMS_BASE_URL}/api/logo-content?populate=image`,
+  );
+
+  return response.json();
+}
+
+export default async function Home() {
+  const logoContent = await getLogoContent();
+
   return (
-    <main className="relative min-h-screen flex items-center justify-center bg-gray-900 overflow-hidden">
-      <div className="grid-background absolute inset-0 p-2 grid grid-cols-12 gap-2 transform -skew-y-12 scale-150">
-        <div className="col-span-2 bg-gray-800 rounded animate-pulse"></div>
-        <div className="col-span-5 bg-gray-800 rounded animate-pulse"></div>
-        <div className="col-span-1 bg-gray-800 rounded animate-pulse"></div>
-        <div className="col-span-4 bg-gray-800 rounded animate-pulse"></div>
-        <div className="col-span-5 bg-gray-800 rounded animate-pulse"></div>
-        <div className="col-span-3 bg-gray-800 rounded animate-pulse"></div>
-        <div className="col-span-2 bg-gray-800 rounded animate-pulse"></div>
-        <div className="col-span-2 bg-gray-800 rounded animate-pulse"></div>
-        <div className="col-span-4 bg-gray-800 rounded animate-pulse"></div>
-        <div className="col-span-7 bg-gray-800 rounded animate-pulse"></div>
-        <div className="col-span-1 bg-gray-800 rounded animate-pulse"></div>
-
-        <div className="col-span-4 bg-gray-800 rounded animate-pulse"></div>
-        <div className="col-span-2 bg-gray-800 rounded animate-pulse"></div>
-        <div className="col-span-2 bg-gray-800 rounded animate-pulse"></div>
-        <div className="col-span-2 bg-gray-800 rounded animate-pulse"></div>
-        <div className="col-span-2 bg-gray-800 rounded animate-pulse"></div>
-        <div className="col-span-2 bg-gray-800 rounded animate-pulse"></div>
-        <div className="col-span-5 bg-gray-800 rounded animate-pulse"></div>
-        <div className="col-span-1 bg-gray-800 rounded animate-pulse"></div>
-        <div className="col-span-4 bg-gray-800 rounded animate-pulse"></div>
-
-        <div className="col-span-4 bg-gray-800 rounded animate-pulse"></div>
-        <div className="col-span-7 bg-gray-800 rounded animate-pulse"></div>
-        <div className="col-span-1 bg-gray-800 rounded animate-pulse"></div>
-
-        <div className="col-span-5 bg-gray-800 rounded animate-pulse"></div>
-        <div className="col-span-1 bg-gray-800 rounded animate-pulse"></div>
-        <div className="col-span-3 bg-gray-800 rounded animate-pulse"></div>
-        <div className="col-span-3 bg-gray-800 rounded animate-pulse"></div>
-        <div className="col-span-2 bg-gray-800 rounded animate-pulse"></div>
-        <div className="col-span-5 bg-gray-800 rounded animate-pulse"></div>
-        <div className="col-span-1 bg-gray-800 rounded animate-pulse"></div>
-        <div className="col-span-4 bg-gray-800 rounded animate-pulse"></div>
-      </div>
-
-      <div className="relative">
-        <h2 className="text-white text-4xl md:text-8xl font-bold flex flex-row items-center">
-          Coming Soon
-        </h2>
-      </div>
+    <main>
+      <Navbar logoContent={logoContent} />
     </main>
   );
 }
