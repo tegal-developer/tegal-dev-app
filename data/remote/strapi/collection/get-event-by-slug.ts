7229e310ceb978be2df25ae84a3e17da37b1cc84
Musthafa Faishal <musthafafaishal@gmail.com>
2024-06-11 14:13:38 +0700

    fix: fix miss cache for detail page

--- data/remote/strapi/collection/get-event-by-slug.ts
+++ data/remote/strapi/collection/get-event-by-slug.ts
@@ -2,6 +2,9 @@ export default async function getEventBySlug(slug: string) {
   try {
     const response = await fetch(
       `${process.env.NEXT_PUBLIC_CMS_API_BASE_URL}/api/events?filters[slug]=${slug}&populate=*`,
+      {
+        cache: 'no-store',
+      },
     );
 
     return response.json();
