3d601f2bcaa7f66d1002c0727e09975e91536a57
Musthafa Faishal <musthafafaishal@gmail.com>
2024-06-16 11:27:41 +0700

    fix: fix

--- data/remote/strapi/collection/get-headline-newest-blogs.ts
+++ data/remote/strapi/collection/get-headline-newest-blogs.ts
@@ -2,9 +2,6 @@ export default async function getHeadlineNewestBlogs() {
   try {
     const response = await fetch(
       `${process.env.NEXT_PUBLIC_CMS_API_BASE_URL}/api/blogs?populate=*`,
-      {
-        cache: 'no-cache',
-      },
     );
 
     return response.json();
--- data/remote/strapi/collection/get-headline-newest-events.ts
+++ data/remote/strapi/collection/get-headline-newest-events.ts
@@ -2,9 +2,6 @@ export default async function getHeadlineNewestEvents() {
   try {
     const response = await fetch(
       `${process.env.NEXT_PUBLIC_CMS_API_BASE_URL}/api/events?populate=*`,
-      {
-        cache: 'no-cache',
-      },
     );
 
     return response.json();
