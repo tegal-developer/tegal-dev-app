411b3545f4c79fcfab89dc45175c75e1fb08d084
Musthafa Faishal <musthafafaishal@gmail.com>
2024-06-22 19:40:10 +0700

    refactor: update section title name

--- app/blogs/authors/[username]/page.tsx
+++ app/blogs/authors/[username]/page.tsx
@@ -1,18 +1,20 @@
 import BlogSection from '@/components/organisms/BlogSection';
 import Template from '@/components/templates/Template';
 import getAllBlogsByAuthorUsername from '@/data/remote/strapi/collection/get-all-blogs-by-author-username';
+import getUserByUsername from '@/data/remote/strapi/collection/get-user-by-username';
 
 export default async function BlogsAuthor({
   params: { username },
 }: {
   params: { username: string };
 }) {
+  const user = await getUserByUsername(username);
   const authorsBlogs = await getAllBlogsByAuthorUsername(username);
 
   return (
     <Template>
       <BlogSection
-        blogHeading={`Blog oleh ${username}`}
+        blogHeading={`Blog oleh ${user[0].name}`}
         headlineNewestBlogs={authorsBlogs?.data}
       />
     </Template>
---
+++ data/remote/strapi/collection/get-user-by-username.ts
@@ -0,0 +1,11 @@
+export default async function getUserByUsername(username: string) {
+  try {
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_CMS_API_BASE_URL}/api/users?filters[username]=${username}`,
+    );
+
+    return response.json();
+  } catch (error) {
+    throw new Error((error as Error).message);
+  }
+}
